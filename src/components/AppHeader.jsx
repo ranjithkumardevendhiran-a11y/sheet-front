a83ebf3dbc7c37fb58edb1d95a28e1d5464c7530
Ranjith Kumar ❤Devendhiran <ranjith.kumardevendhiran@tvs.in>
2026-06-22 09:52:23 +0530

    first commit

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,11 +1,7 @@
 export function LogoBadge({ small = false }) {
   return (
     <div className={`logo-badge${small ? ' logo-badge-small' : ''}`}>
-      <div className="app-logo-mark">TVS</div>
-      <div className="app-logo-text">
-        <span className="brand">myTVS</span>
-        {!small && <span className="tagline">India's Largest Multi-Brand Car Service Network</span>}
-      </div>
+      <img src="/image.png" alt="myTVS" style={{ height: small ? 30 : 50 }} />
     </div>
   );
 }
